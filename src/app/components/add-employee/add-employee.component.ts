d716a4357b86361864373b2155739fbef4d26d41
Radhika <agniradhika@users.noreply.github.com>
2025-09-22 14:14:12 +0200

    add datepicker, selector, buttons to add employee form

--- src/app/components/add-employee/add-employee.component.ts
+++ src/app/components/add-employee/add-employee.component.ts
@@ -6,10 +6,23 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-add-employee',
-  imports: [MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatDatepickerModule],
+  imports: [MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatIconModule
+  ],
+    
+    providers: [provideNativeDateAdapter()],
   templateUrl: './add-employee.component.html',
   styleUrl: './add-employee.component.scss'
 })
@@ -26,7 +39,11 @@ export class AddEmployeeComponent {
 
   saveEmployee() {
     if(this.empForm.valid) {
-
+      console.log("", this.empForm.value)
     }
+  }
+
+  closepopup() {
+
   }
 }
